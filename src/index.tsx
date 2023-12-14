bc307136c03a7cd32358c23b76bdbed6329f7431
pushpraj15295 <pushprajnda007@gmail.com>
2023-12-14 23:57:49 +0530

    remove CRA and use webpack for this project

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Container: HTMLElement | null = document.getElementById("root");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+if (Container) {
+  const root = createRoot(Container);
+  root.render(<App />);
+} else {
+  throw new Error("Target Container is not a dom element");
+}
